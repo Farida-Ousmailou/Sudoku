15e6f85bbd870bb1cbc36fa9749892aec2fbb0b7
ousmai_f <ousmai_f@etna-alternance.net>
2024-04-26 23:06:41 +0200

    ajout de la fonction solve_sudoku

--- main.py
+++ main.py
@@ -15,3 +15,18 @@ def generate_sudoku_grid():
         grid[_][col_indices[0]] = 0
 
     return grid
+
+def solve_sudoku(grid):
+    empty_cell = find_empty_cell(grid)
+    if not empty_cell:
+        return True 
+
+    row, col = empty_cell
+
+    for num in range(1, 10):
+        if is_valid(grid, row, col, num):
+            grid[row][col] = num
+            if solve_sudoku(grid):
+                return True
+            grid[row][col] = 0 
+    return False
