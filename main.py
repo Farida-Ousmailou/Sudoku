bded75de9463d2c9d6f0bae28cd6fbad00b9f05d
Farida-Ousmailou <faridaousmailou51@gmail.com>
2024-04-27 12:36:57 +0200

    add some functions

--- main.py
+++ main.py
@@ -15,3 +15,33 @@ def generate_sudoku_grid():
         grid[_][col_indices[0]] = 0
 
     return grid
+
+def solve_sudoku(grid):
+    empty_cell = find_empty_cell(grid)
+    if not empty_cell:
+        return True 
+
+    row, col = empty_cell
+
+    for num in range(1, 10):
+        if is_valid(grid, row, col, num):
+            grid[row][col] = num
+            if solve_sudoku(grid):
+                return True
+            grid[row][col] = 0 
+    return False
+
+
+def find_empty_cell(grid):
+
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] == 0:
+                return (i,j)
+    return None
+
+def is_valid(grid, row, col, num):
+
+    for i in range(9):
+        if grid[row][i] == num:
+            return False
